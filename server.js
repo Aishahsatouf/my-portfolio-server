c30e79042812f972ac0c3647af253cb8b7c88f73
Aishahsatouf <aishasattouf1996@gmail.com>
2020-11-19 14:38:17 +0200

    My portfolio is ready

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+const express = require('express');
+const server = express();
+server.use(express.static('./public'));
+
+const PORT = process.env.PORT || 3000;
+
+server.get('/test',(request,response)=>{
+    console.log('test route');
+     response.send('your test route is working')
+ })
+
+ server.get('/people',(request,response)=>{
+    let family = [
+        {name:'Razan'},
+        {name:'Abdulrahman'},
+        {name:'Fatima'},
+        {name:'Hanaa'},
+        {name:'Husam'}
+    ];
+    response.json(family);
+})
+
+server.listen(PORT,() => console.log(`Listening on port ${PORT}`));
