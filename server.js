15078a97811b91dc3106ebd477963f0049f827ef
Aishahsatouf <aishasattouf1996@gmail.com>
2023-07-30 03:15:28 +0000

    deployment

--- server.js
+++ server.js
@@ -5,22 +5,22 @@ const express = require('express');
 const server = express();
 server.use(express.static('./public'));
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 server.get('/test',(request,response)=>{
     console.log('test route');
      response.send('your test route is working')
  })
 
- server.get('/people',(request,response)=>{
-    let family = [
-        {name:'Razan'},
-        {name:'Abdulrahman'},
-        {name:'Fatima'},
-        {name:'Hanaa'},
-        {name:'Husam'}
-    ];
-    response.json(family);
-})
+//  server.get('/people',(request,response)=>{
+//     let family = [
+//         {name:'Razan'},
+//         {name:'Abdulrahman'},
+//         {name:'Fatima'},
+//         {name:'Hanaa'},
+//         {name:'Husam'}
+//     ];
+//     response.json(family);
+// })
 
 server.listen(PORT,() => console.log(`Listening on port ${PORT}`));
