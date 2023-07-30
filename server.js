22d6c61ddd9cd33da2197aca2b04741fc191f754
Aishahsatouf <aishasattouf1996@gmail.com>
2023-07-30 16:29:43 +0000

    change main route

--- server.js
+++ server.js
@@ -3,24 +3,13 @@
 
 const express = require('express');
 const server = express();
-server.use(express.static('./'));
+server.use(express.static('./public'));
 
 const PORT = process.env.PORT || 3000;
 
 server.get('/test',(request,response)=>{
-    console.log('test route');
-     response.send('your test route is working')
- })
-
-//  server.get('/people',(request,response)=>{
-//     let family = [
-//         {name:'Razan'},
-//         {name:'Abdulrahman'},
-//         {name:'Fatima'},
-//         {name:'Hanaa'},
-//         {name:'Husam'}
-//     ];
-//     response.json(family);
-// })
+  console.log('test route');
+  response.send('your test route is working')
+})
 
 server.listen(PORT,() => console.log(`Listening on port ${PORT}`));
